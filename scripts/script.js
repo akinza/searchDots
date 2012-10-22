96ceb4cfbad0a646d6cbb89109aec07b7fcdf9c3
Sanjay Rabidas <rabidassanjay@gmail.com>
2012-10-23 02:32:49 +0530

    Added functionality to load color dot in the center of the viewing window

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,20 @@
-$(document).ready(function() {
-  $( "#map-canvas" ).draggable({cursor:'crosshair',containment : [-350,-300,350,100]});
-  $('li.india').click(function() {
-  });
+$(document).ready(function(){
+  $( "#map-canvas" ).draggable({cursor:'crosshair',containment : [-550,-500,550,300]});
 });
+function show(el) {
+  var p = $('.'+el).offset();
+  var w = $('#map-canvas').offset();
+  var pos;
+  pos = translate(p,w);
+  $('#map-canvas').offset({left:pos.left,top:pos.top});
+  console.log("el",el,"p",p,"w",w,"pos",pos);
+  return;
+}
+function translate(point,window) {
+  var px = point.left, py = point.top;
+  var wx = window.left, wy = window.top;
+  var vx = 600, vy = 300;
+  return({ left : wx-px+vx, top : wy-py+vy });
+}
+
+
