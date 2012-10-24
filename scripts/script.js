c88012bee034e33cf6342307a6d563ea0690a1fa
Sanjay Rabidas <rabidassanjay@gmail.com>
2012-10-24 20:36:20 +0530

    Changed Onload autofocus yellow dot

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,11 @@
-$(document).ready(function(){
+$(function(){
   $( "#map-canvas" ).draggable({
     cursor:'crosshair',
     containment : [-550,-500,550,300]
   });
-/*    
-  var dragging = false;
-  var dragger, offsetX, offsetY;
-
-  $("#map-canvas").mousedown(function(e) {
-      dragging = true;
-      dragger = this;
-      offsetX=e.offsetX;
-      offsetY=e.offsetY;
-  });
-
-  $("#map-canvas").mouseup(function(e) {
-      dragging = false;
-  }).mousemove(function(e) {
-    if (dragging) {
-        $(dragger).stop().animate({left:e.pageX-offsetX, top:e.pageY-offsetY},1000);
-        console.log(e.pageX+" "+e.pageY);
-    }
-  });
-  */
+  var itemFocused = "yellow";
+  //$("#"+itemFocused).addClass('focused');
+  show(itemFocused);
 });
 
 function show(el) {
@@ -30,7 +13,7 @@ function show(el) {
   var w = $('#map-canvas').offset();
   var pos;
   pos = translate(p,w);
-//  $('#map-canvas').offset({left:pos.left,top:pos.top});
+  //$('#map-canvas').offset({left:pos.left,top:pos.top});
   $('#map-canvas').stop().animate({left:pos.left,top:pos.top},1000);
   console.log("el",el,"p",p,"w",w,"pos",pos);
   return;
@@ -41,7 +24,6 @@ function translate(point,window) {
   var wx = window.left, wy = window.top;
   var vx = 250, vy = 200;
   return({ left : wx-px+vx, top : wy-py+vy });
-  //return({ left : wx-px, top : wy-py });
 }
 
 
